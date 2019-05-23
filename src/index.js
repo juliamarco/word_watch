4866122cc79901f3a6cd592e5fa950675e3a7df8
Julia Marco <marcootj@gmail.com>
2019-05-23 11:22:23 -0600

    post functionality

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import $ from 'jquery'
 
 $(document).ready(() => {
   getTopWord();
+  $('button').click( () => addWord() );
 })
 
 const getTopWord = () => {
-  let url = 'http://localhost:3000/api/v1/top_word';
+  let url = 'https://wordwatch-api.herokuapp.com/api/v1/top_word';
   fetch(url)
   .then( response => response.json() )
   .then( data => {
@@ -13,5 +14,20 @@ const getTopWord = () => {
     let count = data["word"][word];
     $(".top-word h3").text(`Top word from Word Watch API: ${word}`);
     $("#word-count").text(`Count: ${count}`)
+  })
+}
+
+const addWord = () => {
+  let words = $('#input').val().split(" ");
+  words.forEach( word => postWord(word) );
+  $('#input').val("")
+}
+
+const postWord = (word) => {
+  let url = 'https://wordwatch-api.herokuapp.com/api/v1/words';
+  fetch(url, {
+              method: 'POST',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({ word: {value: word }})
   })
 }
