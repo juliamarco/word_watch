b4947a9fbc4c44af3fb39cdfb7898e6f761b0a30
Julia Marco <marcootj@gmail.com>
2019-05-23 10:11:13 -0600

    Add fetch to get and display top word and count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  getTopWord();
 })
+
+const getTopWord = () => {
+  let url = 'http://localhost:3000/api/v1/top_word';
+  fetch(url)
+  .then( response => response.json() )
+  .then( data => {
+    let word = Object.keys(data["word"]);
+    let count = data["word"][word];
+    $(".top-word h3").text(`Top word from Word Watch API: ${word}`);
+    $("#word-count").text(`Count: ${count}`)
+  })
+}
